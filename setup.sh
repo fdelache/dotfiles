8be59d9e118b7d9c3ca9d791fbabce360e071251
Franck Delache <franck.delache@shopify.com>
2022-01-26 18:12:05 -0500

    Update spin setup

--- setup.sh
+++ setup.sh
@@ -5,10 +5,29 @@ if [[ -z ${SPIN}  ]]; then
 	exit 1
 fi
 
+###############################
+# Install software
+###############################
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# https://github.com/VundleVim/Vundle.vim/issues/511#issuecomment-134251209
+echo | echo | vim +PluginInstall +qall &>/dev/null
+
+# Install fzf
+sudo apt-get install -y fzf
+
+###############################
+# Set personal configuration
+###############################
 [[ ! -f ~/.gitconfig_local ]] && mv ~/.gitconfig ~/.gitconfig_local
 cp gitconfig ~/.gitconfig
 
 cp bash_aliases ~/.bash_aliases
 
-echo "# Read the basic bash_aliases" >> ~/.zshrc
-echo "[[ -f $HOME/.bash_aliases  ]] && source $HOME/.bash_aliases" >> ~/.zshrc
+cp zshrc ~/.zshrc
+cp tmux.conf ~/.tmux.conf
+cp vimrc ~/.vimrc
