9b43e58bbb41a7500f07c52473754b4d44803b91
Franck Delache <franck.delache@shopify.com>
2022-01-27 17:12:50 -0500

    Add missing tools

--- setup.sh
+++ setup.sh
@@ -11,8 +11,23 @@ fi
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting
+brew install zsh-syntax-highlighting
+
+# Install thefuck
+brew install thefuck
+
+# Install pure prompt
+brew install pure
+
 # Install vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install molokai vim theme
+curl -fsSL https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim 
 
 # https://github.com/VundleVim/Vundle.vim/issues/511#issuecomment-134251209
 echo | echo | vim +PluginInstall +qall &>/dev/null
