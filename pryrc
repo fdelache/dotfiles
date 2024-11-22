8b07ecdbd790508013dc274a185423504c8881f2
Franck Delache <franck.delache@shopify.com>
2024-11-22 14:37:38 -0500

    Cleanup old configuration files

--- pryrc
+++
@@ -1,7 +0,0 @@
-# Returns a filtered backtrace, omitting Rails/Sorbet layers.
-
-def bt
-	caller.select do |line|
-		line.start_with?("/home/spin/src/github.com/Shopify/shopify/components") && !line.include?("/instrumentation/")
-	end
-end
--- setup.sh
+++ setup.sh
@@ -17,47 +17,5 @@ for file in "$(pwd)"/zshrc.d/*.sh; do
 done
 
 # Configure git
-[[ ! -f ~/.gitconfig_local ]] && mv ~/.gitconfig ~/.gitconfig_local
 ln -s "$(pwd)/gitconfig" ~/.gitconfig
-
-# exit early for now
-exit 0
-
-###############################
-# Install software
-###############################
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install zsh-autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Install pure prompt
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
-
-# Install vim plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Install molokai vim theme
-mkdir -p ~/.vim/colors
-curl -fsSL https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim --output ~/.vim/colors/molokai.vim
-
-# https://github.com/VundleVim/Vundle.vim/issues/511#issuecomment-134251209
-echo | echo | vim +PluginInstall +qall &>/dev/null
-
-###############################
-# Set personal configuration
-###############################
-[[ ! -f ~/.gitconfig_local ]] && mv ~/.gitconfig ~/.gitconfig_local
-ln -s $(pwd)/gitconfig ~/.gitconfig
-
-ln -s $(pwd)/bash_aliases ~/.bash_aliases
-
-[[ -e ~/.zshrc ]] && mv ~/.zshrc ~/.zshrc.backup
-ln -s $(pwd)/zshrc ~/.zshrc
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/pryrc ~/.pryrc
+ln -s "$(pwd)/gitignore" ~/.gitignore
